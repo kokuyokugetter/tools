f1d4e8e221db4098dc9f2b02ec712875e74f101e
kokuyokugetter <kokuyokugetter@gmail.com>
2021-01-18 12:05:46 +0900

    bmi追加

--- js/test.js
+++ js/test.js
@@ -1,1 +1,12 @@
-alert('hello, hello')
+jQuery( function() {
+    getValue();
+    var $formObjectInputs = jQuery( '#bmiForm :input' );
+    $formObjectInputs.change( function() { getValue(); } );
+    $formObjectInputs.keyup( function() { getValue(); } );
+} );
+function getValue() {
+    var $formObject = jQuery( '#bmiForm' );
+    var height_m = ($formObject.find( '[name=formHeight]' ).val()) / 100
+    var bmi = $formObject.find( '[name=formWeight]' ).val() / height_m / height_m
+    jQuery( '#bmiValue' ).html( bmi );
+}
