e5559cc067e2bc596869bb1081ee0b1b5cbb4ecb
kokuyokugetter <kokuyokugetter@gmail.com>
2021-01-20 09:55:13 +0900

    インチ変換追加

--- js/test.js
+++ js/test.js
@@ -1,12 +1,34 @@
+// BMI
 jQuery( function() {
-    getValue();
+    setBmi();
     var $formObjectInputs = jQuery( '#bmiForm :input' );
-    $formObjectInputs.change( function() { getValue(); } );
-    $formObjectInputs.keyup( function() { getValue(); } );
+    $formObjectInputs.change( function() { setBmi(); } );
+    $formObjectInputs.keyup( function() { setBmi(); } );
 } );
-function getValue() {
+function setBmi() {
     var $formObject = jQuery( '#bmiForm' );
     var height_m = ($formObject.find( '[name=formHeight]' ).val()) / 100
     var bmi = $formObject.find( '[name=formWeight]' ).val() / height_m / height_m
     jQuery( '#bmiValue' ).html( bmi );
+}
+
+// センチインチ変換
+jQuery( function() {
+    convertCenti();
+    var $formObjectInputInch = jQuery( '#inch-centiForm :input[name=formInch]' );
+    $formObjectInputInch.change( function() { convertCenti(); } );
+    $formObjectInputInch.keyup( function() { convertCenti(); } );
+    var $formObjectInputCenti = jQuery( '#inch-centiForm :input[name=formCenti]' );
+    $formObjectInputCenti.change( function() { convertInch(); } );
+    $formObjectInputCenti.keyup( function() { convertInch(); } );
+} );
+function convertCenti() {
+    var $formObject = jQuery( '#inch-centiForm' );
+    var centi = ($formObject.find( '[name=formInch]' ).val()) * 2.54
+    jQuery( '#inch-centiForm :input[name=formCenti]' ).val( centi );
+}
+function convertInch() {
+    var $formObject = jQuery( '#inch-centiForm' );
+    var inch = ($formObject.find( '[name=formCenti]' ).val()) / 2.54
+    jQuery( '#inch-centiForm :input[name=formInch]' ).val( inch );
 }
