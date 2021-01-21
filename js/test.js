4024622224b237ef571c338c276addc6b1d7e8f8
kokuyokugetter <kokuyokugetter@gmail.com>
2021-01-21 17:35:31 +0900

    元号何年の判定追加

--- js/test.js
+++ js/test.js
@@ -32,3 +32,40 @@ function convertInch() {
     var inch = ($formObject.find( '[name=formCenti]' ).val()) / 2.54
     jQuery( '#inch-centiForm :input[name=formInch]' ).val( inch );
 }
+
+// 元号何年計算(平成、令和のみ)
+jQuery( function() {
+    inputnow();
+    CalJapaneseEra();
+    var $formObjectInputInch = jQuery( '#yearForm :input[name=year]' );
+    $formObjectInputInch.change( function() { CalJapaneseEra(); } );
+    $formObjectInputInch.keyup( function() { CalJapaneseEra(); } );
+} );
+function inputnow() {
+    var now = new Date();
+    var year = now.getFullYear();
+    jQuery( '#yearForm :input[name=year]' ).val( year );
+}
+function CalJapaneseEra() {
+    var $formObject = jQuery( '#yearForm' );
+    var year = $formObject.find( '[name=year]' ).val()
+    var diff_heisei = year - 1988
+    var diff_reiwa = year - 2018
+    if (diff_heisei < 1) {
+        jQuery( '#era' ).html( "1989〜2050年のみ対応" );
+        jQuery( '#yearValue' ).html( "" );
+        return 0;
+      }
+    if (year == 2019) {
+        jQuery( '#era' ).html( "令和1年" );
+        jQuery( '#yearValue' ).html( "平成31年" );
+        return 0;
+    }
+    if (diff_reiwa > 0) {
+        jQuery( '#era' ).html( "令和" );
+        jQuery( '#yearValue' ).html( `${diff_reiwa}年` );
+        return 0;
+    }
+    jQuery( '#era' ).html( "平成" );
+    jQuery( '#yearValue' ).html( `${diff_heisei}年` );
+}
